403ace9389f26e4c85a8f17a02764e439657944b
Paul Thimote <pthimote@gmail.com>
2022-09-19 16:29:01 -0400

    commit tree f0be795c966c778fb9ee6ac21bdc84f6f6c7a068

--- index.js
+++ index.js
@@ -28,21 +28,31 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+// let cow, horse, sheep, pig, chicken;
+// [cow, horse, sheep, pig, chicken] = ['mo', 'snort', 'baah', 'oink','warble']
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const animalNames = 'cow sheep pig chicken'
+const [bessie, dolly, babe, little] = animalNames.split(' ')
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const animalColors = 'cow sheep pig'
+const [blackAndWhite, black, pink] = animalColors.split(' ')
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+const colorCode = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const [red, orange, yellow, green, blue, indigo, violet] = colorCode
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+const colorLetter = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+const [r, o, y, g, b, v] = colorLetter
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+const ind = ['indigo']
+const [indg] = ind
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
-
+const object = {muppetName:'Miss Piggy', color:'pink', song:'Never Before, Never Again', job:'Cast member of The Muppet Show', partner:'Kermit'}
+const {muppetName, color, song, job, partner} = object
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const object2 = {song2:'Moving Right Along', song4:'I Hope That Something Better Comes Along', nestedJob:'Host of The Muppet Show', nestedPartner:'Miss Piggy'}
+const {song2, song4, nestedJob, nestedPartner} = object2
